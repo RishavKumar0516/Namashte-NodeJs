3efcb16a916f8593b32b120cf93e6d181eaf88e6
Lappydeko <iamrishavkumaar@hotmail.com>
2026-09-06 23:32:06 +0530

    added notes

--- 4_get ready to node js/calculate/index.js
+++ 4_get ready to node js/calculate/index.js
@@ -1,6 +1,8 @@
 const { multiplyNum } = require('./multiplyNum');
 const { calculateSum } = require('../calculateSum')
 
+
+// using this app.js will don't need to know that
 module.exports = {
     multiplyNum,
     calculateSum
