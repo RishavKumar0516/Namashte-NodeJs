28d66a09cbd9a07b855a44323add08e49a3bf563
Lappydeko <iamrishavkumaar@hotmail.com>
2026-09-08 00:55:26 +0530

    learn the internals of require and module.exports

---
+++ 5_Module explaination/app.js
@@ -0,0 +1,37 @@
+
+
+// you cannot access the variable and function defined inside the outer function, outside of the outer function
+
+function outer() {
+    let x = 50;
+    function inner() {
+        console.log("inside inner function");
+    }
+}
+
+outer();  //this is doable
+
+inner() // this is not doable
+console.log("x", x); // this is not doable
+
+
+// the variable and function defined inside the function are in the local scope of that function, cannot be accessed outside of the function.
+
+// whenever you create a module, all the code written inside the module get wrapped inside the functon and then get executed, so the only way to use the function and variable defined inside the module is to export it using module.exports object.
+
+// when you do require('./calculateSum'), the node will wrap the whole module code inside the function and then execute it.
+
+// this function is not a normal function it is a IIFE function. (Immediately Invoked Function Expression)
+
+// using IIFE function because, it doesn't interfare the other code.
+
+// How are variable and function are private in different module?
+// its because IIFE and require function, it keeps function and variable safe.
+
+// how you get access to module.exports?
+// nodeJs passes the module object as parameter to the IIFE function
+
+
+
+
+
---
+++ 5_Module explaination/calculate/index.js
@@ -0,0 +1,9 @@
+const { multiplyNum } = require('./multiplyNum');
+const { calculateSum } = require('../calculateSum')
+
+
+// using this app.js will don't need to know that
+module.exports = {
+    multiplyNum,
+    calculateSum
+}
